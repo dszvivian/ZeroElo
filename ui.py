60243259f24a94125ab4011764d3b01f4b325d34
dszvivian <dszvivian@gmail.com>
2023-09-20 01:23:09 +0530

    engine returns first move after each user move

--- ui.py
+++ ui.py
@@ -3,15 +3,25 @@ import chess.svg
 
 board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") #Load the starting position
 
+# Returns a first move on each type
+def getFirstMove(board):
+    for l in board.legal_moves:
+        global a
+        a = str(l)
+        break
+    return a
 
-# basic pass N play chess 
+ 
 while(True):
     userMove = input()
-    move = chess.Move.from_uci(str(userMove))
-    board.push(move)
-
+    user_move = chess.Move.from_uci(str(userMove))
+    board.push(user_move)
+    engine_move = chess.Move.from_uci(getFirstMove(board))
+    board.push(engine_move)
     print(board)
+
     
+
 
 
 
