329555fd6293a0e842236d4218364333de663a92
dszvivian <dszvivian@gmail.com>
2023-09-20 23:59:36 +0530

    Termination Conditions Done

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 import chess
-import chess.svg
 
-board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") #Load the starting position
+board = chess.Board("3r4/8/R7/2P1krp1/3p4/1B3R2/P7/2K2n2 w - - 4 57") #Load the starting position
+print(board)
 
 # Returns a first move on each type
 def getFirstMove(board):
@@ -11,15 +11,17 @@ def getFirstMove(board):
         break
     return a
 
- 
-while(True):
-    userMove = input()
+
+while not board.is_game_over():
+    userMove = input("Enter the move: (ie:e2e4)")
     user_move = chess.Move.from_uci(str(userMove))
     board.push(user_move)
-    engine_move = chess.Move.from_uci(getFirstMove(board))
-    board.push(engine_move)
-    print(board)
-
+    if(not board.is_game_over()):
+        engine_move = chess.Move.from_uci(getFirstMove(board))
+        board.push(engine_move)
+        print(board)
+    else:
+        print(board)
     
 
 
